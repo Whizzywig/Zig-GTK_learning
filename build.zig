f91a940b4dd63817351205edb74ab2966c7f4683
Benjamin Rockley <whizzywig.rockley@gmail.com>
2023-11-29 16:27:31 +0000

    remove broken windows build linking

--- build.zig
+++ build.zig
@@ -12,56 +12,16 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     
-    //gtk import - can't find .lib files as they don't exist on windows
-    //maybe test building on linux
     exe.linkLibC();
 
-    if (b.args) |args| {
-        //pass "windows" if building on windows
-        if (args.next == "windows"){
-            exe.linkSystemLibrary("comctl32");
-            exe.linkSystemLibrary("gdi32");
-            exe.linkSystemLibrary("gdiplus");
-            //link the gtk stuff
-            //This doesn't work because it is of tpye .typelib and zig can't link it
-            exe.addLibraryPath(.{ .path = "C:/msys64/mingw64/lib/girepository-1.0"});
-            exe.linkSystemLibrary("gtk4");
-            exe.linkSystemLibrary("gdk4");
-            exe.addLibraryPath(.{ .path = "C:/msys64/mingw64/lib"});
-            exe.linkSystemLibrary("gobject-2.0");
-            exe.linkSystemLibrary("gio-2.0");
-            exe.linkSystemLibrary("pangoft2-1.0");
-            exe.linkSystemLibrary("gdk_pixbuf-2.0");
-            exe.linkSystemLibrary("pango-1.0");
-            exe.linkSystemLibrary("cairo");
-            exe.linkSystemLibrary("pangocairo-1.0");
-            exe.linkSystemLibrary("freetype");
-            exe.linkSystemLibrary("fontconfig");
-            exe.linkSystemLibrary("gmodule-2.0");
-            exe.linkSystemLibrary("gthread-2.0");
-            exe.linkSystemLibrary("glib-2.0");
-
-            exe.linkSystemLibrary("ffi");
-            exe.linkSystemLibrary("gettextlib");
-            exe.linkSystemLibrary("intl");
-
-            //Find the include directory on windows
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/gtk-4.0"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/glib-2.0"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/lib/glib-2.0/include"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/cairo"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/pango-1.0"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/harfbuzz"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/gdk-pixbuf-2.0"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include/graphene-1.0"});
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/lib/graphene-1.0/include"});
-            
-            exe.addIncludePath(.{ .path = "C:/msys64/mingw64/include"});
-        } else {
-            //link for any other operating system
-            exe.linkSystemLibrary("gtk4");
-        }
-    }
+    ///TODO: Find a way to tell people who build on windows that it won't work only cross compiling for windows could work
+        
+    //link for any other operating system
+    exe.addLibraryPath(.{ .path = "/bin"});
+    exe.addLibraryPath(.{ .path = "/usr/bin"});
+    exe.addLibraryPath(.{ .path = "/usr/local/bin"});
+    exe.addIncludePath(.{ .path = "/usr/include"});
+    exe.linkSystemLibrary("gtk4");
     
     b.installArtifact(exe);
 
